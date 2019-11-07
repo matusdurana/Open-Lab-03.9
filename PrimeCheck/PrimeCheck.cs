032555ed8341d58637328f138144aec84ea7a921
z18ista03 <z18ista03@Z00P1-08.SPSKNM>
2019-11-07 09:14:37 +0100

    Add project files.

---
+++ PrimeCheck/PrimeCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeCheck
+{
+    class PrimeCheck
+    {
+        public bool IsPrimeNumber(int num)
+        {
+            if (num < 2)
+                return false;
+
+            if (num == 2)
+                return true;
+
+            for (var i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+        
+            return true;
+        }
+    }
+}
---
+++ PrimeCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your number:");
+            int num = int.Parse(Console.ReadLine());
+
+            PrimeCheck primecheck = new PrimeCheck();
+            primecheck.IsPrimeNumber(num);
+
+            bool write = primecheck.IsPrimeNumber(num);
+            Console.WriteLine(write);
+        }
+    }
+}
